315e46d21ebf95520182d1ba5fb43d8ea56b706d
Reece Pohlman <reecepohlman@gmail.com>
2023-03-07 22:16:24 -0500

    added links to projects

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -3,21 +3,27 @@ import React from 'react';
 function PortfolioItem({title, imgUrl, stack, link}){
     return(
         <div id='portfolioloItem' className='border-2 border-stone-900 dark:border-white rounded-lg overflow-hidden'>
-            <img 
-                src={imgUrl}
-                alt='portfolio'
-                className='w-full h-36 md:h-48 object-cover cursor-pointer'
-            />
-            <div className='w-full p-4'>
-                <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold dark:text-white'>{title}</h3>
-                <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-small'>
-                    {stack.map(item => (
-                        <span className='inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-lg dark:border-white'>
-                            {item}
-                        </span>
-                    ))}
-                </p>
-            </div>
+            <a 
+                href={link}
+                target='_blank'
+                rel='noopener noreferrer'
+                >
+                <img
+                    src={imgUrl}
+                    alt='portfolio'
+                    className='w-full h-36 md:h-48 object-cover cursor-pointer'
+                />
+                <div className='w-full p-4'>
+                    <h3 className='text-lg md:text-xl mb-2 md:mb-3 font-semibold dark:text-white'>{title}</h3>
+                    <p className='flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-small'>
+                        {stack.map(item => (
+                            <span className='inline-block px-2 py-1 font-semibold border-2 border-stone-900 rounded-lg dark:border-white'>
+                                {item}
+                            </span>
+                        ))}
+                    </p>
+                </div>
+            </a>
         </div>
     )
 }
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -3,24 +3,24 @@ export default [
         title: 'Homework tracker',
         imgUrl: '/assets/homework-log.png',
         stack: ['HTML','CSS','C#', 'ASP.NET'],
-        link:''
+        link: 'https://www.github.com/pohlman24/DnDBot'
     },
     {
         title: 'Etch-a-Sketch',
         imgUrl: '/assets/etch-a-sketch.png',
         stack: ['HTML','CSS','Javascript'],
-        link:''
+        link:'https://pohlman24.github.io/Etch-a-Sketch/'
     },
     {
         title: 'D&D Discord bot',
         imgUrl: '/assets/DnD-bot.png',
         stack: ['Python', 'Web Scrapping', 'Discord API'],
-        link:''
+        link: 'https://www.github.com/pohlman24/DnDBot'
     },
     {
         title: 'Minimal Portfolio',
         imgUrl: './assets/portfolio.png',
         stack: ['React','Tailwind','Vite'],
-        link:''
+        link: 'https://reecepohlman-portfolio.netlify.app/'
     }
 ]
