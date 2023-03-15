66a227bf2a0be14031cfc7b4e63704017af21284
Reece Pohlman <reecepohlman@gmail.com>
2023-03-14 20:54:25 -0400

    added resume icon to footer and linked to resume pdf

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-
 function Footer(){
     return (
 		<div className="py-5 text-center">
@@ -43,6 +43,21 @@ function Footer(){
 									/>
 								</g>
 								</svg>
+							</a>
+							<a 
+								href="public\assets\Reece_Pohlman_Resume.pdf"
+                                target="_blank"
+								className="bg-stone-400 p-2 font-semibold text-white inline-flex items-center space-x-2 rounded-full">
+								<svg 
+									xmlns="http://www.w3.org/2000/svg" 
+									width="16" 
+									height="16" 
+									fill="currentColor" 
+									class="bi bi-file-earmark-fill" 
+									viewBox="0 0 16 16"
+								>
+  									<path d="M4 0h5.293A1 1 0 0 1 10 .293L13.707 4a1 1 0 0 1 .293.707V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2zm5.5 1.5v2a1 1 0 0 0 1 1h2l-3-3z"/>
+								</svg>				
 							</a>
 						</div>
 					</div>
